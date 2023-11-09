ce4abb2b4d619266c17a865bbe8993552bf531b4
quasimontecarlo <info@crltt.com>
2023-11-09 19:41:36 +1030

    advanced footer to a decent stage, added logo icon and resized everything properly. Wrote little javascript to redirect to random curated webpaged when logo is clicked. will need to troubleshoot issues when scaling. Need to check navbar next

--- script/script.js
+++ script/script.js
@@ -1,19 +1,33 @@
 // add class navbarDark on navbar scroll
-console header = document.quarySelector('.navbar');
-console.log(header)
+
+const header = document.querySelector('.navbar');
 window.onscroll = function() {
 	const top = window.scrollY;
-	if(top >=100) {
+	if (top >=100) {
 		header.classList.add('navbarDark');
 	}
 	else {
 		header.classList.remove('navbarDark');
 	}
 }
+
 // collapse navbar after click on small devices
 const navLinks = document.querySelectorAll('.nav-item')
-const menuToggle = document.getElementById('navSupportContent')
+const menuToggle = document.getElementById('navbarSupportedContent')
 
 navLinks.forEach((l) => {
-	l.addEventListener('click', () => { new bootstrap.collapse(menuToggle).toggle() })
+	l.addEventListener('click', () => { new bootstrap.Collapse(menuToggle).toggle() })
 })
+
+
+// random website link
+
+var rb = document.getElementById("logo_button");
+rb.addEventListener("click", randweb);
+
+function randweb() {
+	var sites = ["https://www.hackertyper.com/", "https://onefishstudio.net/ucg", "https://neal.fun/password-game/", "https://pointerpointer.com/", "https://mondrianandme.com/", "https://www.agegeek.com/", "https://www.yourmetricbirthday.com/", "https://thezen.zone/", "https://papertoilet.com/", "https://jurassicsystems.com/", "https://tvtropes.org/", "https://www.keybr.com/"]
+	var rs = sites[Math.floor(Math.random() * sites.length)];
+	console.log(rs);
+	window.open(rs, "_blank").focus();
+}
