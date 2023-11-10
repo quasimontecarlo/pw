7374d259f0d9b2bdcd2e084706a3aa836aa4637d
crltt <info@crltt.com>
2023-11-10 16:19:58 +1030

    completed main page, navbar, about, wrkd_on and footer sections done

--- script/script.js
+++ script/script.js
@@ -1,5 +1,5 @@
 // add class navbarDark on navbar scroll
-
+/*
 const header = document.querySelector('.navbar');
 window.onscroll = function() {
 	const top = window.scrollY;
@@ -19,7 +19,7 @@ navLinks.forEach((l) => {
 	l.addEventListener('click', () => { new bootstrap.Collapse(menuToggle).toggle() })
 })
 
-
+*/
 // random website link
 
 var rb = document.getElementById("logo_button");
@@ -28,6 +28,5 @@ rb.addEventListener("click", randweb);
 function randweb() {
 	var sites = ["https://www.hackertyper.com/", "https://onefishstudio.net/ucg", "https://neal.fun/password-game/", "https://pointerpointer.com/", "https://mondrianandme.com/", "https://www.agegeek.com/", "https://www.yourmetricbirthday.com/", "https://thezen.zone/", "https://papertoilet.com/", "https://jurassicsystems.com/", "https://tvtropes.org/", "https://www.keybr.com/"]
 	var rs = sites[Math.floor(Math.random() * sites.length)];
-	console.log(rs);
 	window.open(rs, "_blank").focus();
 }
