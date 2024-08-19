974e06dda1c84f49ae7ea56c677912c17386c687
quasimontecarlo <info@crltt.com>
2024-08-19 13:58:07 +0930

    adding tln, of course

--- script/script.js
+++ script/script.js
@@ -26,7 +26,7 @@ var rb = document.getElementById("logo_button");
 rb.addEventListener("click", randweb);
 
 function randweb() {
-	var sites = ["https://www.hackertyper.com/", "https://onefishstudio.net/ucg", "https://neal.fun/password-game/", "https://pointerpointer.com/", "https://mondrianandme.com/", "https://www.agegeek.com/", "https://www.yourmetricbirthday.com/", "https://thezen.zone/", "https://papertoilet.com/", "https://jurassicsystems.com/", "https://tvtropes.org/", "https://www.keybr.com/"]
+	var sites = ["https://www.hackertyper.com/", "https://onefishstudio.net/ucg", "https://neal.fun/password-game/", "https://pointerpointer.com/", "https://mondrianandme.com/", "https://www.agegeek.com/", "https://www.yourmetricbirthday.com/", "https://thezen.zone/", "https://papertoilet.com/", "https://jurassicsystems.com/", "https://tvtropes.org/", "https://www.keybr.com/", "https://www.thelonelynetwork.com/"]
 	var rs = sites[Math.floor(Math.random() * sites.length)];
 	window.open(rs, "_blank").focus();
 }
